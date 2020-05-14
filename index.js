3fccf471c65f2ad30f610a7aa531cb3765e8ea0e
AustenHsiao <austenhsiao@gmail.com>
2020-05-13 20:04:16 -0700

    file name change

---
+++ index.js
@@ -0,0 +1,9 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    const original = req.query.text;
+    const snapshot = await admin.database().ref('/chatLogs').push({original: original});
+    res.redirect(303, snapshot.ref.toString());
+  });
